b4866a7b533a5a613b6336e02ffb90a9afc24b0a
Martin <martinbruun1995@gmail.com>
2024-06-11 13:58:33 +0200

    refactor: Update calendarTable schema in calendar.ts

--- src/db/schema/calendar.ts
+++ src/db/schema/calendar.ts
@@ -1,11 +1,9 @@
-import { drizzle } from 'drizzle-orm/vercel-postgres';
-import { sql } from '@vercel/postgres';
-import { pgTable, serial, text, timestamp, uniqueIndex } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, timestamp, date } from "drizzle-orm/pg-core"
 
-export const CalendarTable = pgTable('entries', {
-  id: serial('id').primaryKey(),
-  title: text('title').notNull(),
-  notes: text('notes'),
-  date: timestamp('date').notNull(),
-  createdAt: timestamp('createdAt').defaultNow().notNull(),
-});
+export const calendarTable = pgTable("entries", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  notes: text("notes"),
+  date: date("date").notNull(),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+})
