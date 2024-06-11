4fe1072636a9c10f66bf1021894730e4a7b4ccd7
Martin <martinbruun1995@gmail.com>
2024-06-11 13:58:49 +0200

    feat: Update page.tsx to fetch and display calendar entries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import CalendarPicker from '@/components/calendar/calendar';
+import { getEntries } from "@/actions"
+import CalendarPicker from "@/components/calendar/calendarPicker"
+import Entries from "@/components/calendar/entries"
 
-export default function Home() {
-  return <CalendarPicker></CalendarPicker>;
+export default async function Home() {
+  const data = await getEntries()
+  return (
+    <section>
+      <CalendarPicker></CalendarPicker>
+      <Entries entries={data}></Entries>
+    </section>
+  )
 }
