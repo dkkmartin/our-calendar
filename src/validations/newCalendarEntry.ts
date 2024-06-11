a1fa476e16a80cb1a3d06496e6658ec7938ad303
Martin <martinbruun1995@gmail.com>
2024-06-11 13:58:24 +0200

    feat: Add validation schema for new calendar entry form

---
+++ src/validations/newCalendarEntry.ts
@@ -0,0 +1,9 @@
+import { z } from "zod"
+
+export const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title must contain at least 1 character",
+  }),
+  notes: z.string().optional(),
+  date: z.date(),
+})
