d75022a8a3c2777eb3412c33ceb2d0450d4b4243
Martin <martinbruun1995@gmail.com>
2024-06-13 13:57:27 +0200

    refactor: Update drizzle.config.ts for database connection URL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,13 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import * as dotenv from "dotenv"
+import { defineConfig } from "drizzle-kit"
+
+dotenv.config()
 
 export default defineConfig({
-  schema: './src/db/schema/*',
-  out: './src/db',
-  dialect: 'postgresql',
+  schema: "./src/db/schema/*",
+  out: "./src/db/migrations",
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.NEON_DATABASE_URL!,
   },
-});
+})
