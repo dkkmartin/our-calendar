94d54ea5f86913d8805a56bb7799abdddc10a660
Martin <martinbruun1995@gmail.com>
2024-06-13 13:57:59 +0200

    refactor: Add newEvent component for creating calendar entries

---
+++ src/components/calendar/newEvent.tsx
@@ -0,0 +1,78 @@
+"use client"
+
+import { createEntry } from "@/actions"
+import { formSchema } from "@/validations/newCalendarEntry"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation"
+
+export default function NewEvent({ date }: { date: string }) {
+  const router = useRouter()
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      notes: "",
+      date: new Date(date),
+    },
+  })
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+    createEntry(values.title, values.notes ? values.notes : "", values.date)
+    form.reset({
+      title: "",
+      notes: "",
+    })
+    router.push("/")
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+        <FormField
+          control={form.control}
+          name='title'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormDescription>The title of the event.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='notes'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Notes</FormLabel>
+              <FormControl>
+                <Textarea {...field} />
+              </FormControl>
+              <FormDescription>Any notes for the event.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  )
+}
