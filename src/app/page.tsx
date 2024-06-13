7749fb97ef4de97ba36ffe13105b672b97a0585f
Martin <martinbruun1995@gmail.com>
2024-06-13 13:58:06 +0200

    refactor: Remove unused code and update CalendarPicker component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import { getEntries } from "@/actions"
 import CalendarPicker from "@/components/calendar/calendarPicker"
 import Entries from "@/components/calendar/entries"
 
-export default async function Home() {
-  const data = await getEntries()
+export default function Home() {
   return (
     <section>
       <CalendarPicker></CalendarPicker>
-      <Entries entries={data}></Entries>
+      <Entries></Entries>
     </section>
   )
 }
