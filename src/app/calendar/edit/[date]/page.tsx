4d4f9e78fba7b5e7b8561a849da7931012667ad1
Martin Bruun <martinbruun1995@gmail.com>
2024-10-16 01:24:39 +0200

    feat: edit a event page

---
+++ src/app/calendar/edit/[date]/page.tsx
@@ -0,0 +1,11 @@
+import NewEvent from "@/components/calendar/newEvent"
+
+interface CalendarEditProps {
+  params: {
+    date: string
+  }
+}
+
+export default function CalendarEdit({ params }: CalendarEditProps) {
+  return <section></section>
+}
