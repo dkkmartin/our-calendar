a9a7bb3b2797aa055a635b32b4071cfebe035177
Martin Bruun <martinbruun1995@gmail.com>
2024-10-16 01:22:53 +0200

    refactor: added indicator for events on calendar dates

--- src/components/calendar/calendarPicker.tsx
+++ src/components/calendar/calendarPicker.tsx
@@ -1,15 +1,24 @@
 "use client"
 
 import { Calendar } from "@/components/ui/calendar"
-import { Button } from "@/components/ui/button"
-import { Plus } from "lucide-react"
-import Link from "next/link"
-import { formatISO } from "date-fns"
 import { currentPickedDateStore } from "@/lib/stores"
+import { useEffect, useState } from "react"
+import { entryType } from "@/types/entryType"
+import { getAllEntries } from "@/actions"
+import NewEventButton from "./newEventButton"
 
 export default function CalendarPicker() {
   const pickedDateStore = currentPickedDateStore((state) => state.date)
   const setPickedDateStore = currentPickedDateStore((state) => state.setDate)
+  const [entries, setEntries] = useState<entryType[]>()
+
+  useEffect(() => {
+    async function getEntries() {
+      const data = await getAllEntries()
+      setEntries(data)
+    }
+    getEntries()
+  }, [])
 
   const handleSelect = (newState: Date | undefined) => {
     if (newState) {
@@ -17,25 +26,31 @@ export default function CalendarPicker() {
     }
   }
 
+  const eventDates = entries?.map((entry) =>
+    new Date(entry.date).toDateString()
+  )
+
   return (
     <section className='flex flex-col items-center'>
       <Calendar
         mode='single'
         selected={pickedDateStore}
         onSelect={handleSelect}
-        className='border-2 border-black rounded max-w-[280px] flex justify-center m-2'
+        className='w-screen'
+        modifiers={{
+          event: (date) => eventDates?.includes(date.toDateString()) || false,
+        }}
+        components={{
+          DayContent: ({ date }) => (
+            <div className='relative w-full h-full flex items-center justify-center'>
+              <span>{date.getDate()}</span>
+              {eventDates?.includes(date.toDateString()) && (
+                <div className='absolute bottom-1 left-1/2 transform -translate-x-1/2 w-1 h-1 bg-blue-500 rounded-full p-1' />
+              )}
+            </div>
+          ),
+        }}
       ></Calendar>
-      {pickedDateStore && (
-        <Link
-          href={`/calendar/${formatISO(pickedDateStore, {
-            representation: "date",
-          })}`}
-        >
-          <Button>
-            <Plus></Plus>
-          </Button>
-        </Link>
-      )}
     </section>
   )
 }
