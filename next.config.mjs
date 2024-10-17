709b286d680c515ea26453038ec3a94f413e2f44
Martin Bruun <martinbruun1995@gmail.com>
2024-10-18 00:11:29 +0200

    refactor: removed locales

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ["en", "da_DK"],
-    defaultLocale: "en",
-  },
   images: {
     remotePatterns: [{ hostname: "img.clerk.com" }],
   },
