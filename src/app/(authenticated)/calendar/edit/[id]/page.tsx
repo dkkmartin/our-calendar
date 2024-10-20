fc64924abacefb3bd734a1ecef7d1e70e7144710
Martin Bruun <martinbruun1995@gmail.com>
2024-10-20 23:15:23 +0200

    refactor: event response changed

--- src/app/(authenticated)/calendar/edit/[id]/page.tsx
+++ src/app/(authenticated)/calendar/edit/[id]/page.tsx
@@ -9,10 +9,13 @@ interface CalendarEditProps {
 
 export default async function CalendarEdit({ params }: CalendarEditProps) {
   const event = await getSpecificEntry(params.id)
-
+  const eventData = event.data?.[0]
+  if (!eventData) {
+    return <div>Event not found</div>
+  }
   return (
     <section>
-      <EditEvent id={params.id} event={event[0]} />
+      <EditEvent id={params.id} event={eventData} />
     </section>
   )
 }
