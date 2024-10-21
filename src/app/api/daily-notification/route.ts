5d52b2ad67970708d3b43b6fad3422578d6019ff
Martin Bruun <martinbruun1995@gmail.com>
2024-10-21 20:42:46 +0200

    refactor: route handler for cron-job

--- src/app/api/daily-notification/route.ts
+++ src/app/api/daily-notification/route.ts
@@ -1,5 +1,42 @@
-import { NextRequest, NextResponse } from "next/server"
+import { sendNotification } from "@/actions"
+import db from "@/db/drizzle"
+import { calendarTable } from "@/db/schema/calendar"
+import { and, eq } from "drizzle-orm"
 
-export default async function GET(req: NextRequest) {
-  return NextResponse.json({ message: "" })
+export async function GET(req: Request) {
+  const authHeader = req.headers.get("Authorization")
+  if (authHeader !== process.env.CRON_SECRET) {
+    return Response.json({ error: "Unauthorized" }, { status: 401 })
+  }
+
+  try {
+    const entries = await db
+      .select()
+      .from(calendarTable)
+      .where(
+        and(
+          eq(calendarTable.notificationEnabled, true),
+          eq(
+            calendarTable.notificationDate,
+            new Date().toISOString().split("T")[0]
+          )
+        )
+      )
+
+    const notificationPromises = entries.map((entry) => {
+      if (entry.userId && entry.title) {
+        return sendNotification(entry.userId, entry.title, entry.notes || "")
+      }
+    })
+
+    await Promise.all(notificationPromises)
+
+    return Response.json({ message: "Success" })
+  } catch (error) {
+    console.error("Error sending daily notification:", error)
+    return Response.json(
+      { error: "Failed to send daily notification" },
+      { status: 500 }
+    )
+  }
 }
