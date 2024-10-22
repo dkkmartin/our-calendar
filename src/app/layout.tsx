7a9825653e748c8d227015e534ec1b9d8667b3d7
Martin Bruun <martinbruun1995@gmail.com>
2024-10-22 20:10:59 +0200

    refactor: moved clerk provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import { ClerkProvider } from "@clerk/nextjs"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
+import { ThemeProvider } from "@/components/theme/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -17,10 +17,17 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <ClerkProvider>
-      <html lang='en'>
-        <body className={cn("", inter.className)}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang='en'>
+      <body className={cn("", inter.className)}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
   )
 }
